b1c2f0c4ab2bbbd04fa43161f47b51338905c6df
Yurybar93 <barynin.yuri@gmail.com>
2024-10-15 19:46:09 +0200

    Update operations_de.py

--- operations_de.py
+++ operations_de.py
@@ -1,5 +1,6 @@
 import database
 
+# Funktion zur Anzeige des Hauptmenüs der Anwendung
 def menu_de():
     return('''Willkommen bei der Filmsuch-App!
 
@@ -10,22 +11,32 @@ Wählen Sie eine Aktion:
 4. Beenden Sie die Anwendung
 
 Geben Sie eine Aktionsnummer ein: _''')
+
+# Funktion zur Suche von Filmen nach Stichwort
 def search_by_keyword_de(cursor, keyword):
+    # SQL-Abfrage zur Suche von Filmen basierend auf einem Stichwort
     query = '''SELECT title, year, imdb_rating FROM movies
 WHERE title LIKE %s OR plot LIKE %s OR genres LIKE %s OR cast LIKE %s OR directors LIKE %s OR year LIKE %s 
 ORDER BY imdb_rating DESC'''
+    # Speichert die Benutzeranfrage für die Analyse
     database.add_user_query(keyword)
+    # Vorbereitung der Parameter für die SQL-Abfrage
     param = f'%{keyword}%'
-    cursor.execute(query, (param, param, param,param,param,param))
+    # Ausführung der Abfrage
+    cursor.execute(query, (param, param, param, param, param, param))
     result = cursor.fetchall()
+    # Überprüfung, ob Filme gefunden wurden
     if not result:
         print('Es wurden keine Filme für Ihren Suchbegriff gefunden.')
     else:
-        print('Filme nach Ihrem Stichwort')
+        print('Filme nach Ihrem Stichwort:')
         for row in result:
             print(f'{row[0]}({row[1]})')
     return result
+
+# Funktion zur Suche nach Filmen nach Genre und Jahr der Veröffentlichung
 def search_by_genres_year_de(cursor, genre, year):
+    # SQL-Abfrage zur Suche nach Genre und Jahr
     query = '''SELECT title, year, imdb_rating FROM movies
 WHERE genres LIKE %s AND year = %s
 ORDER BY imdb_rating DESC'''
@@ -37,6 +48,7 @@ ORDER BY imdb_rating DESC'''
     
     cursor.execute(query, (genre_param, year))
     result = cursor.fetchall()
+    # Rückgabe des Ergebnisses oder einer Nachricht, wenn keine Übereinstimmungen gefunden wurden
     if not result:
         return 'Es wurden keine Filme gefunden, die Ihren Parametern entsprechen.'
     else:
@@ -44,6 +56,8 @@ ORDER BY imdb_rating DESC'''
         for row in result:
             response += f'{row[0]} ({row[1]}) - Bewertung IMDB: {row[2]}\n'
         return response
+
+# Funktion zum Sortieren der gefundenen Filme
 def sort_de(result):
     print("Wählen Sie den zu sortierenden Parameter aus:")
     print("1. Nach Name")
@@ -51,6 +65,7 @@ def sort_de(result):
     print("3. Wie von IMDb bewertet")
     
     choice = input("Geben Sie die Nummer des zu sortierenden Parameters ein: ")
+    # Sortierung basierend auf der Benutzerwahl
     if choice == '1':
         sorted_results = sorted(result, key=lambda x: x[0])
     elif choice == '2':
@@ -59,26 +74,34 @@ def sort_de(result):
         sorted_results = sorted(result, key=lambda x: x[2], reverse=True)
     else:
         print("Falsche Wahl.")
+
+    # Ausgabe sortierter Filme mit Farbanzeige basierend auf der Bewertung
     for row in sorted_results:
         title, year, rating = row
         rating = float(rating)
         if rating > 7.5:
-            color = '\033[92m' 
+            color = '\033[92m'  # grün für Bewertungen über 7.5
         elif 5 <= rating <= 7.5:
-            color = '\033[93m' 
+            color = '\033[93m'  # gelb für Bewertungen zwischen 5 und 7.5
         else:
-            color = '\033[91m'  
+            color = '\033[91m'  # rot für Bewertungen unter 5
         print(f'{color}{title}({year}) {rating}\033[0m')
+
+# Funktion um detaillierte Informationen zu einem Film zu erhalten
 def get_movie_details_de(cursor):
     title = input('Geben Sie den Titel des Films ein, um Einzelheiten zu erfahren:')
+    # SQL-Abfrage, um Filmdetails zu erhalten
     query = '''SELECT title, year, genres, plot, imdb_rating, directors, cast FROM movies WHERE title = %s'''
     cursor.execute(query, (title,))
     movie_details = cursor.fetchone()
+    
     import re
-    genres = ', '.join(str(genre) for genre in movie_details[2])
-    directors = re.sub(r'[\[\]""]', '', movie_details[5])
-    cast = re.sub(r'[\[\]""]', '', movie_details[6])
+    # Bereinigung der Daten von überflüssigen Zeichen
     if movie_details:
+        genres = ', '.join(str(genre) for genre in movie_details[2])
+        directors = re.sub(r'[$$$$""]', '', movie_details[5])
+        cast = re.sub(r'[$$$$""]', '', movie_details[6])
+        
         print("Weitere Informationen über den Film:")
         print(f"Titel: {movie_details[0]}")
         print(f"Jahr: {movie_details[1]}")
@@ -89,10 +112,14 @@ def get_movie_details_de(cursor):
         print(f"Schauspieler: {cast}")
     else:
         print("Film nicht gefunden.")
+
+# Funktion zum Schließen der Datenbankverbindung
 def close_de(cursor, connection):
     cursor.close()
     connection.close()
     return 'Auf Wiedersehen! Kommen Sie wieder!'
+
+# Funktion um beliebte Suchanfragen zu erhalten
 def get_popular_queries(cursor):
     query = """
         SELECT keyword, COUNT(*) as count
@@ -104,19 +131,18 @@ def get_popular_queries(cursor):
     
     cursor.execute(query)
     
-    # Fetch all results
+    # Abruf aller Ergebnisse
     result = cursor.fetchall()
     
-
-    # Check if result is empty
+    # Überprüfung, ob das Ergebnis leer ist
     if not result:
         return "Keine populären Anfragen."
 
     response = 'Beliebte Suchanfragen:\n'
     
-    # Iterate through the result
+    # Iteration durch das Ergebnis
     for row in result:
         response += f'{row[0]} - {row[1]} mal\n'
 
-   
     return response
+``
